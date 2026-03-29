9e5576a8bb75bfb955d2f96f5d1e4b33c12fd0b9
gche3 <gche3@illinois.edu>
2026-03-28 22:05:56 -0500

    scene modifications: small, medium large clutter; fire on stove for scene 2

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+[ExecuteAlways]
+public class LightFlicker : MonoBehaviour
+{
+    public float minIntensity = 0.5f;
+    public float maxIntensity = 1.5f;
+    public float flickerSpeed = 0.1f;
+
+    private Light lightSource;
+    private float baseIntensity;
+
+    void Start()
+    {
+        lightSource = GetComponent<Light>();
+        if (lightSource != null)
+        {
+            baseIntensity = lightSource.intensity; 
+            InvokeRepeating("Flicker", 0.0f, flickerSpeed); 
+        }
+    }
+
+    void Flicker()
+    {
+        lightSource.intensity = baseIntensity * Random.Range(minIntensity, maxIntensity);
+    }
+}
